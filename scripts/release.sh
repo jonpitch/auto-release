2586df927baf37dc1d66734da29bb2b7d1baac02
jonpitch <jon.pitcherella@gmail.com>
2017-09-02 18:31:04 -0400

    attempt at pushing back to origin

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  # setup git
-  # git config --global user.email "travis@travis-ci.org"
-  # git config --global user.name "Travis CI"
-  #
-  # git remote add master https://${GH_TOKEN}@github.com/jonpitch/auto-release.git > /dev/null 2>&1
-  # git push --quiet --set-upstream master master
-
   # cut tag
   npm run release
 
