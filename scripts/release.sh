c571997909ed49d5ac0a3399290fded4c82da480
jonpitch <jon.pitcherella@gmail.com>
2017-09-03 09:39:25 -0400

    only stage changelog and setup origin ahead of tag

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  # cut tag
-  npm run release
-
-  # update changelog and push to master
   git remote rm origin
   git remote add origin https://jonpitch:${GH_TOKEN}@github.com/jonpitch/auto-release
   git fetch
   git checkout master
+
+  # cut tag
+  npm run release
+
+  # update changelog and push to master
   npm run changelog
   git push --set-upstream origin master --follow-tags
   exit 0
