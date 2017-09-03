6ad8142533d2f196b7c8be054ee80852175b62df
Jon Pitcherella <jon.pitcherella@gmail.com>
2017-09-03 11:13:07 -0400

    Single commit (#12)
    * before commit hook
    
    * beforeCommit attempt
    
    * revert to master version

--- config/release.js
+++ config/release.js
@@ -1,21 +1,28 @@
 /* jshint node:true */
-// var RSVP = require('rsvp');
+var RSVP = require('rsvp');
+const { exec } = require('child_process');
 
 // For details on each option run `ember help release`
 module.exports = {
   local: true,
   // remote: 'some_remote',
   annotation: "%@ [skip ci]",
-  message: "%@ [skip ci]"
+  message: "%@ [skip ci]",
   // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
   // publish: true,
   // strategy: 'date',
   // format: 'YYYY-MM-DD',
   // timezone: 'America/Los_Angeles',
   //
-  // beforeCommit: function(project, versions) {
-  //   return new RSVP.Promise(function(resolve, reject) {
-  //     // Do custom things here...
-  //   });
-  // }
+  beforeCommit: function(/* project, versions */) {
+    return new RSVP.Promise(function(resolve, reject) {
+      exec('auto-changelog -t compact --package', (err) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve();
+      });
+    });
+  }
 };
